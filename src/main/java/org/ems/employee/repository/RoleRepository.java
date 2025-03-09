2a63c4eda1788593d617eb33a27b14362aa345df
devnsk <devnsk.2024@gmail.com>
2025-03-09 16:13:03 +0530

    findByRoleNameAndRoleIdNot method added

--- src/main/java/org/ems/employee/repository/RoleRepository.java
+++ src/main/java/org/ems/employee/repository/RoleRepository.java
@@ -1,8 +1,13 @@
 package org.ems.employee.repository;
 
+import java.util.Optional;
+
+import org.ems.employee.model.Employee;
 import org.ems.employee.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role,Long>{
+
+	Optional<Role> findByRoleNameAndRoleIdNot(String roleName, Long roleId);
 
 }
