80e4a8d529cd76de0d4bb79b2e47a2c964da7f75
devnsk <devnsk.2024@gmail.com>
2025-03-09 16:09:38 +0530

    mapped with multiple employee instances

--- src/main/java/org/ems/department/model/Department.java
+++ src/main/java/org/ems/department/model/Department.java
@@ -1,64 +1,81 @@
 package org.ems.department.model;
 
+import java.util.List;
+
+import org.ems.employee.model.Employee;
+import org.hibernate.annotations.GenericGenerator;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import org.hibernate.annotations.GenericGenerator;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Department {
-    @Id
-    @GeneratedValue(generator = "custom-generator")
-    @GenericGenerator(name = "custom-generator", strategy = "org.ems.utils.CustomIdGenerator")
-    private String departmentId;
-    private String departmentName;
-    private String description;
+	@Id
+	@GeneratedValue(generator = "custom-generator")
+	@GenericGenerator(name = "custom-generator", strategy = "org.ems.utils.CustomIdGenerator")
+	private String departmentId;
+	private String departmentName;
+	private String description;
+	@OneToMany(mappedBy = "department")
+	private List<Employee> employees;
 
-    public Department(String departmentId, String departmentName, String description) {
-        super();
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.description = description;
-    }
+	public Department(String departmentId, String departmentName, String description) {
+		super();
+		this.departmentId = departmentId;
+		this.departmentName = departmentName;
+		this.description = description;
+	}
 
-    public Department() {
-        super();
-    }
+	public Department(String departmentId, String departmentName, String description, List<Employee> employees) {
+		super();
+		this.departmentId = departmentId;
+		this.departmentName = departmentName;
+		this.description = description;
+		this.employees = employees;
+	}
 
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId='" + departmentId + '\'' +
-                ", departmentName='" + departmentName + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
+	public List<Employee> getEmployees() {
+		return employees;
+	}
 
-    public String getDepartmentId() {
-        return departmentId;
-    }
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
 
-    public String getDepartmentName() {
-        return departmentName;
-    }
+	public Department() {
+		super();
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	@Override
+	public String toString() {
+		return "Department{" + "departmentId='" + departmentId + '\'' + ", departmentName='" + departmentName + '\''
+				+ ", description='" + description + '\'' + '}';
+	}
 
-    public void setDepartmentId(String departmentId) {
-        this.departmentId = departmentId;
-    }
+	public String getDepartmentId() {
+		return departmentId;
+	}
 
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
+	public String getDepartmentName() {
+		return departmentName;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
+	public void setDepartmentId(String departmentId) {
+		this.departmentId = departmentId;
+	}
 
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
 
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
 }
