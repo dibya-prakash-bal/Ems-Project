3c2532a70c823817136b64b4140e80645ec02fbb
devnsk <devnsk.2024@gmail.com>
2025-03-09 16:17:49 +0530

    Methods written to find employees throgh roleid and deptid

--- src/main/java/org/ems/employee/repository/EmployeeRepository.java
+++ src/main/java/org/ems/employee/repository/EmployeeRepository.java
@@ -1,8 +1,12 @@
 package org.ems.employee.repository;
 
+import java.util.List;
+
 import org.ems.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-	
+	List<Employee> findByDepartmentDepartmentId(String departmentId);
+
+	List<Employee> findByRoleRoleId(Long roleId);
 }
