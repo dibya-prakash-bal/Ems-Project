d52abda50979f12e3c80f9a4d993468ec5496e33
devnsk <devnsk.2024@gmail.com>
2025-03-09 16:10:37 +0530

    findByDepartmentName method added

--- src/main/java/org/ems/department/repository/DepartmentRepository.java
+++ src/main/java/org/ems/department/repository/DepartmentRepository.java
@@ -1,10 +1,11 @@
 package org.ems.department.repository;
 
+import java.util.Optional;
+
 import org.ems.department.model.Department;
-import org.ems.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, String> {
 
-
+	  Optional<Department> findByDepartmentName(String deptName);
 }
